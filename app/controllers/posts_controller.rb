1e25c1256159362f078bf52977fa3e0b1dc47722
Riley Moore <rilemoore@gmail.com>
2019-12-13 23:49:18 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  
   def create
     Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
