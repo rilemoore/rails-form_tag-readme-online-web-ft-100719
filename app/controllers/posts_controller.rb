11cc54421b9f5ab039744fb588980470e1a25eb9
Riley Moore <rilemoore@gmail.com>
2019-12-13 23:54:49 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,8 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
   
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
 end
